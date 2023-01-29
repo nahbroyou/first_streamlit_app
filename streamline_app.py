e4b63d24ce9f5e8dd963bec799bd21d5308a5a7c
nahbroyou <tugboat00.tenants@icloud.com>
2023-01-29 16:51:02 -0600

    api to dataframe

--- streamline_app.py
+++ streamline_app.py
@@ -22,7 +22,13 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do?
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+#streamlit.text(fruityvice_response.json())
 
 #api key for food site: dRb3cEd8uNU9Mbl8FqyU3g0YJNIDWWiqXSOIDsH4
 # https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=dRb3cEd8uNU9Mbl8FqyU3g0YJNIDWWiqXSOIDsH4
