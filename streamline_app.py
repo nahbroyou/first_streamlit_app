5814cdc9414d0f1650db300426dda5740e038c59
nahbroyou <tugboat00.tenants@icloud.com>
2023-01-29 22:35:25 -0600

    error control

--- streamline_app.py
+++ streamline_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -21,15 +22,28 @@ fruits_to_show = my_fruit_list.loc[fruits_selected] #display the table on the pa
 streamlit.dataframe(fruits_to_show)
 
 
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input ('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?")
+if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
+
 
 # write your own comment -what does the next line do?
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+
+#don't run anything past here while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -37,3 +51,6 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input ('Add a fruit to the list?')
+streamlit.write('The user entered', add_my_fruit)
